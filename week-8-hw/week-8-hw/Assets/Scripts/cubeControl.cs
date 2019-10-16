49a0b12674a32c2835ca6b9c79cf89c024f008b1
Dylan Nanjad <dnanjad00@gmail.com>
2019-10-16 00:39:16 -0400

    added week-8-hw
    floating spheres with sound

---
+++ week-8-hw/week-8-hw/Assets/Scripts/cubeControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeControl : MonoBehaviour
+{
+    int i;
+    public GameObject[] cubes;
+    public GameObject audioManager;
+
+
+    void Update()
+    {
+        
+        for (i = 0; i < 4; i++)
+        {
+            //float cubes up and down
+            cubes[i].transform.position = new Vector3(cubes[i].transform.position.x, Mathf.Sin(Time.time), 0);
+        }
+
+    //check if Space pressed
+    IfSpacePlaySound();
+
+    }
+
+    void IfSpacePlaySound()
+{
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+        //grab audio from another object
+        AudioSource source = audioManager.GetComponent<AudioSource>();
+        source.Play();
+    }
+}
+}
