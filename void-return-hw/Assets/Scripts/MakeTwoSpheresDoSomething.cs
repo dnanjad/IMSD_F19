f2c07edc78af10226794bc5929f20df9092d998a
Dylan Nanjad <dnanjad00@gmail.com>
2019-11-12 23:55:42 -0500

    did void and return hw

---
+++ void-return-hw/Assets/Scripts/MakeTwoSpheresDoSomething.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeTwoSpheresDoSomething : MonoBehaviour
+{
+    Transform myChildTheFirstSphere;
+    Transform myChildTheSecondSphere;
+
+    float timer;
+
+    void Start()
+    {
+        myChildTheFirstSphere = transform.GetChild(0);
+        myChildTheSecondSphere = transform.GetChild(1);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 5)
+            timer = 0;
+
+        if (timer >= 0 && timer < 2.5f)
+        {
+            //Do something with the first sphere
+            MoveATransformUpward(myChildTheFirstSphere);
+            Debug.Log(TheHeightOfThisGameObject(myChildTheFirstSphere.gameObject));
+        }
+
+        if (timer >= 2.5f && timer < 5.0f)
+        {
+            //Do something with the second sphere
+            MoveATransformUpward(myChildTheSecondSphere);
+            Debug.Log(TheHeightOfThisGameObject(this.gameObject));
+        }
+    }
+
+    void MoveATransformUpward(Transform theTransformThatIWantToMoveUpward)
+    {
+        theTransformThatIWantToMoveUpward.position += Vector3.up * Time.deltaTime;
+    }
+
+    float TheHeightOfThisGameObject(GameObject g)
+    {
+        float theHeight = g.transform.position.y;
+        return theHeight;
+    }
+}
